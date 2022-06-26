4c4f82161362b110afe2ad9e4bf95334800a7c6d
Alex Kiura <kiuraalex@gmail.com>
2022-06-26 12:42:15 +0300

    Add http response class

--- pronto/response.py
+++ pronto/response.py
@@ -14,14 +14,16 @@ class HttpResponse:
         status_code: int = 200,
         headers: Optional[Mapping[str, str]] = None
     ) -> None:
-        pass
+        self.body = body
+        self.connection = connection
+        self.status_code = status_code
+        self.headers = headers
 
     def __await__(self):
-        pass
-
-
-class JsonResponse(HttpResponse):
-    def __init__(
-        self, data: Any, connection: Optional[Connection] = None, *args, **kwargs
-    ):
-        pass
+        if not self.connection:
+            raise ValueError("No connection")
+        self.connection.response_status_code = self.status_code
+        if self.headers:
+            for key, value in self.headers.items():
+                self.connection.insert_response_header(key, value)
+        return self.connection.send(self.body, finish=True).__await__()
